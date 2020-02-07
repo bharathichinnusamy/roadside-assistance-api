c68a2190e5df58f66b6cdbee35993a739df6b457
bharathichinnusamy <lcsbharathi@gmail.com>
2020-02-07 16:23:44 +0000

    changed user deatils dynamically for sms

--- src/main.py
+++ src/main.py
@@ -257,7 +257,7 @@ def handle_incident():
         # its time to send the sms to everyone at this postal code
         heros_nearby = Hero.query.filter(Hero.zip_code==postal_code)
         for _hero in heros_nearby:
-            send_sms("Hello"+_hero.first_name+", someone needs your help! please reply with "+str(fourthstep.incident_id)+" if you are willing to help", _hero.phone)
+            send_sms("Hello "+_hero.first_name+", someone needs your help! please reply with "+str(fourthstep.incident_id)+" if you are willing to help", _hero.phone)
             return "success"
             
 @app.route('/incident/response',methods=['POST'])
@@ -274,7 +274,9 @@ def receive_test_sms():
     db.session.merge(newone)
     db.session.commit()
     resp=MessagingResponse()
-    resp.message("Thank you,here's user's details\nFirst Name:Bharathi\nLast Name:Chinnusamy\nhttp://maps.google.com/?q=40.714224,-73.961452")
+    userdetail=User.query.filter(User.user_id==newone.user_id)
+
+    resp.message("Thank you,here's a user's details..\nFirst Name:userdetail.first_name\nPhone No:userdetail.phone\nhttp://maps.google.com/?q=40.714224,-73.961452")
     return str(resp),200
 
 
