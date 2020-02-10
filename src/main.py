44a49ff86e89ed3e6b6fffa64de044a7dafca0ca
bharathichinnusamy <lcsbharathi@gmail.com>
2020-02-10 07:58:23 +0000

    cors issue

--- src/main.py
+++ src/main.py
@@ -280,7 +280,8 @@ def handle_incident():
         for _hero in heros_nearby:
             print(_hero)
             send_sms("Hello "+_hero.first_name+", someone needs your help! please reply with "+str(fourthstep.incident_id)+" if you are willing to help", _hero.phone)
-            return "success"
+            response.headers.add("Access-Control-Allow-Origin", "*")
+            return response
             
 @app.route('/incident/response',methods=['POST'])
 def receive_test_sms(): 
